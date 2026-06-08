51e2db1fe2a2f9017af369290ed81a1c34f7c24d
1mache <miktor4925@gmail.com>
2026-06-08 21:36:32 +0300

    Added dummy drawable layer sorting

--- src/system/RenderSystem.cpp
+++ src/system/RenderSystem.cpp
@@ -2,6 +2,8 @@
 #include "Components.h"
 #include "RenderContext.h"
 #include "Transform.h"
+#include <algorithm>
+#include <vector>
 #include <SDL3/SDL.h>
 #include <bagel.h>
 #include <cmath>
@@ -35,18 +37,36 @@ void drawCupLiquid(SDL_Renderer* r, const SDL_FRect& cupRect, const Cup& c)
 }
 } // namespace
 
-void drawSystem()
+void drawSystem(SDL_Renderer* renderer)
 {
+    using Entity = bagel::Entity;
+
     static const bagel::Mask mask =
         bagel::MaskBuilder().set<Drawable>().set<Transform>().build();
 
-    SDL_Window*   window   = RenderContext::getWindow();
-    SDL_Renderer* renderer = RenderContext::getRenderer();
-    for (auto e = bagel::Entity::first(); !e.eof(); e.next())
+    std::vector<Entity> drawables{};
+    for (auto e = Entity::first(); !e.eof(); e.next())
     {
         if (!e.test(mask))
             continue;
 
+        drawables.emplace_back(e);
+    }
+
+    // TODO: replace dummy sorting with performant storage type
+    auto layerLess = [](const Entity a, const Entity b)
+    {
+        const auto& da = a.get<Drawable>();
+        const auto& db = b.get<Drawable>();
+
+        return da.renderLayer < db.renderLayer;
+    };
+
+    // sort based on render layer
+    std::ranges::sort(drawables, layerLess);
+
+    for (auto e: drawables)
+    {
         const auto& d = e.get<Drawable>();
         const auto& t = e.get<Transform>();
 
