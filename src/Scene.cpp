c050a25787f93f2406c8b17a6aef642fad25ec50
1mache <miktor4925@gmail.com>
2026-06-11 03:12:46 +0300

    Scene is now polymorphic with template methods

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -7,5 +7,18 @@ void Scene::init(SDL_Renderer* renderer)
 {
     _renderer = renderer;
     _assetManager.init(renderer);
+
+    onInit();
+}
+void Scene::run()
+{
+    // can add things here
+    onRun();
+    // and here!
+}
+void Scene::cleanup()
+{
+    onCleanup();
+    _assetManager.clear();
 }
 } // namespace cafe
--- src/Scene.h
+++ src/Scene.h
@@ -3,15 +3,26 @@
 
 namespace cafe
 {
-class Scene final
+class Scene
 {
 public:
     explicit Scene(std::string_view bgTexturePath)
     : _bgTexturePath(bgTexturePath)
     {}
+
+    // template method pattern
     void init(SDL_Renderer* renderer);
+    void run();
+    void cleanup();
+
     AssetManager& getAssetManager() { return _assetManager; }
     const Texture& getBgTexture() { return _assetManager.getTexture(_bgTexturePath);}
+
+    virtual ~Scene() = default;
+protected:
+    virtual void onInit()    = 0;
+    virtual void onRun()     = 0;
+    virtual void onCleanup() = 0;
 private:
     const std::string _bgTexturePath;
     SDL_Renderer*     _renderer{};
