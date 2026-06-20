3b877969da4aa14480041b6df4cfa4e4d4ccb1ee
1mache <miktor4925@gmail.com>
2026-06-20 10:16:51 +0300

    More customers

--- src/SpriteDims.h
+++ src/SpriteDims.h
@@ -4,7 +4,6 @@
 namespace cafe
 {
 static constexpr SDL_FPoint CUP_DIMS      = {24, 24};
-static constexpr SDL_FPoint MINI_CUP_DIMS = {16, 16};
 static constexpr SDL_FPoint PROP_DIMS     = {16, 16};
 static constexpr SDL_FPoint PERSON_DIMS   = {32, 48};
 static constexpr SDL_FPoint BUTTON_DIMS   = {4,4};
